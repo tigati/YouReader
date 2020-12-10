5294ca9a4b3277775ac2f5ebc190ee9fd62d4471
Timur Gayfulin <tiga83@gmail.com>
2020-12-10 19:12:45 +0300

    Initial Commit

---
+++ YouReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouReader
+//
+//  Created by Timur Gayfulin on 10.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
